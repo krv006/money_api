c99bf318437ce29f97d199eb8eea32b96b1424f1
“krv006” <rvkamronbek@gmail.com>
2024-03-17 02:29:04 +0500

    money_api

---
+++ core/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Category, Card, Consumption, Sub_category
+
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta :
+        model = Category
+        fields = ['name', 'info', 'sub_category']
+
+
+class CardSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Card
+        fields = ['plastic_name']
+
+
+class ConsumptionSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Consumption
+        fields = ['name', 'info', 'cost', 'sub_category', 'time', 'status', 'date']
+
+
+class Sub_categorySerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Sub_category
+        fields = ['name']
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .models import Category, Card, Sub_category, Consumption
+from .serializers import CategorySerializer, CardSerializer, Sub_categorySerializer, ConsumptionSerializer
+# from .serializers import 
+
+class CategoryListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryListAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+
+class CardListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CardSerializer
+
+class CardListAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CardSerializer
+
+
+
+class Sub_categoryListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = Sub_categorySerializer
+
+class Sub_categoryListAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = Sub_categorySerializer
+
+
+
+class ConsumptionListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = ConsumptionSerializer
+
+class ConsumptionListAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = ConsumptionSerializer
